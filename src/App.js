fbbddd1e7b8232d109d284ca30bb4ef3b0579028
Jeronimo <jerotrica@gmail.comm>
2022-10-06 01:27:33 -0300

    Core React Concepts, useState Hook module

--- src/App.js
+++ src/App.js
@@ -1,32 +1,12 @@
-import React from "react";
-import { render } from "react";
-const Pet = (props) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, props.name),
-    React.createElement("h2", {}, props.animal),
-    React.createElement("h2", {}, props.breed),
-  ]);
-};
-
+import { render } from "react-dom";
+import SearchParams from "./SearchParams";
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", { id: "brand" }, "Adopt Me!"),
-    React.createElement(Pet, {
-      name: "Luna",
-      animal: "Dog",
-      breed: "Havanese",
-    }),
-    React.createElement(Pet, {
-      name: "Pepper",
-      animal: "Bird",
-      breed: "Cockatiel",
-    }),
-    React.createElement(Pet, {
-      name: "Pasita",
-      animal: "Dog",
-      breed: "Labrador",
-    }),
-  ]);
+  return (
+    <div id="my-app">
+      <h1>Adopt Me!</h1>
+      <SearchParams />
+    </div>
+  );
 };
 
-render(React.createElement(App), document.getElementById("root"));
+render(<App />, document.getElementById("root"));
