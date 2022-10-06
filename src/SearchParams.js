2ac44e1eebe779f66e3642b1c0ae5be0a4258fa1
Jeronimo <jerotrica@gmail.comm>
2022-10-06 18:03:25 -0300

    Module 4 finished, going to Effects Module

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,13 +1,68 @@
+import { useState } from "react";
+
+const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
+
 const SearchParams = () => {
-  const location = "Seattle, WA";
+  const [location, setLocation] = useState("");
+  const [animal, setAnimal] = useState("");
+  const [breed, setBreed] = useState("");
+  const breeds = [];
+
   return (
     <div className="search-params">
       <form>
         <label htmlFor="location">
           Location
-          <input id="location" value={location} placeholder="location" />
+          <input
+            id="location"
+            value={location}
+            placeholder="location"
+            onChange={(e) => setLocation(e.target.value)}
+          />
         </label>
-        <button className="test">Submit</button>
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={animal}
+            onChange={(e) => {
+              setAnimal(e.target.value);
+              setBreed("");
+            }}
+            onBlur={(e) => {
+              setAnimal(e.target.value);
+              setBreed("");
+            }}
+          >
+            <option />
+            {ANIMALS.map((animal) => (
+              <option key={animal} value={animal}>
+                {animal}
+              </option>
+            ))}
+          </select>
+        </label>
+        <label htmlFor="breed">
+          Breed
+          <select
+            id="breed"
+            value={breed}
+            onChange={(e) => {
+              setBreed(e.target.value);
+            }}
+            onBlur={(e) => {
+              setBreed(e.target.value);
+            }}
+          >
+            <option />
+            {breeds.map((breeds) => (
+              <option key={breeds} value={breeds}>
+                {breeds}
+              </option>
+            ))}
+          </select>
+        </label>
+        <button>Submit</button>
       </form>
     </div>
   );
