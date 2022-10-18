3459da1aac561d320441d65379efd68a837472ba
Jeronimo <jerotrica@gmail.comm>
2022-10-18 18:50:34 -0300

    Created first custom hook

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import { render } from "react-dom";
 
-import Bel from "./Bel";
-//import SearchParams from "./SearchParams";
+import SearchParams from "./SearchParams";
+
 const App = () => {
   return (
     <div id="my-app">
       <h1>Adopt Me!</h1>
-      <Bel />
+      <SearchParams />
     </div>
   );
 };
